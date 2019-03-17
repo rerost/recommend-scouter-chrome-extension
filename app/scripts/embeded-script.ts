609cd8a4a3ee3b4858dd4d2a2be0c62e651de830
rerost <gmailii@yahoo.co.jp>
2019-03-17 15:30:07 +0900

    Show json anyways

--- app/scripts/embeded-script.ts
+++ app/scripts/embeded-script.ts
@@ -2,7 +2,25 @@ let elements = window.document.getElementsByClassName("recommend-scouter-item");
 
 for (let i = 0; i < elements.length; i++) {
   // TODO: Show scouter
-  window.console.log(i);
+  let elem:Element = elements[i]
+  let dataScoreDescription: any = getScoreDescription(elem)
+  if (dataScoreDescription == null) {
+    continue
+  }
+  showScouter(dataScoreDescription)
+}
+
+function showScouter(scoreDescription: any) {
+  window.console.log(scoreDescription)
+}
+
+function getScoreDescription(elem: Element): any | null {
+  let dataScoreDescription: string | null = elem.getAttribute("data-score-description");
+  if (dataScoreDescription == null) {
+    return null
+  }
+
+  return JSON.parse(dataScoreDescription)
 }
 
 window.console.log(elements);

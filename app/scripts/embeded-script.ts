c2f552f0f57c7982a0253f0157fd9d5da14be76a
rerost <gmailii@yahoo.co.jp>
2019-03-17 17:12:08 +0900

    Check only class name

--- app/scripts/embeded-script.ts
+++ app/scripts/embeded-script.ts
@@ -1,16 +1,17 @@
 import * as bundle from "../../api/bundle"
 import pb = bundle.com.github.rerost.recommend_scouter.api
 
-let elements = window.document.getElementsByClassName("recommend-scouter-item");
-
-for (let i = 0; i < elements.length; i++) {
-  let elem:Element = elements[i]
+window.document.addEventListener("click", (ev: MouseEvent) => {
+  let elem = ev.toElement
+  if (elem.className != "recommend-scouter-item") {
+    return
+  }
   let dataScoreDescription: any | null = getScoreDescription(elem)
   if (dataScoreDescription == null) {
-    continue
+    return
   }
   showScouter(dataScoreDescription)
-}
+})
 
 function showScouter(scoreDescription: pb.ScoreDescription) {
   window.console.log(scoreDescription)
@@ -25,5 +26,3 @@ function getScoreDescription(elem: Element): pb.ScoreDescription | null {
   let json: any = JSON.parse(dataScoreDescription)
   return pb.ScoreDescription.fromObject(json)
 }
-
-window.console.log(elements);
